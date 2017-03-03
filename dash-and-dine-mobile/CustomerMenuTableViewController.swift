5d725c7260557fe8501ad0227717e37f34d155d2
matthew-gordon <laxredemption5525@gmail.com>
2017-03-02 20:16:20 -0700

    Update Restaurant view, add CustomerMenuView controller

---
+++ dash-and-dine-mobile/CustomerMenuTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomerMenuTableViewController.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/2/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import UIKit
+
+class CustomerMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ dash-and-dine-mobile/RestaurantViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RestaurantViewController.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/2/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBarButton.target = self.revealViewController()
+        menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+    }
+}
+
+extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
