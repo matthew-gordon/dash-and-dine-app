2ed153c8353f291c4916bb932200c19841834748
matthew-gordon <laxredemption5525@gmail.com>
2017-03-03 09:58:25 -0700

    Add card payment layout

---
+++ dash-and-dine-mobile/PaymentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentViewController.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/3/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class PaymentViewController: UIViewController {
+
+    @IBOutlet weak var cardTextField: STPPaymentCardTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
