9f78797c96b88c4603561d9b25749d53997e8039
matthew-gordon <laxredemption5525@gmail.com>
2017-03-02 22:56:30 -0700

    Add Meal Details view/controller, map view

---
+++ dash-and-dine-mobile/MealDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MealDetailsViewController.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/2/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
