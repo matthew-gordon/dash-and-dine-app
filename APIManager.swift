9aff1e55781c7dc7859ff2ff9a48560fdec97bbc
matthew-gordon <laxredemption5525@gmail.com>
2017-03-04 16:50:00 -0700

    Add login/logout API

--- APIManager.swift
+++ APIManager.swift
@@ -15,20 +15,71 @@ class APIManager {
 
     static let shared = APIManager()
     
-    let baseURL = NSURL(string: "localhost:8000/")
+    let baseURL = NSURL(string: BASE_URL)
     
     var accessToken = ""
     var refreshToken = ""
     var expired = Date()
     
     // API to login a User
-    func login(userType: String, completionHandler: @escaping (JSON) -> Void) {
+    func login(userType: String, completionHandler: @escaping (NSError?) -> Void) {
     
+        let path = "api/social/convert-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String: Any] = [
+            "grant_type": "conevrt-token",
+            "client_id": CLIENT_ID,
+            "client_Secret": CLIENT_SECRET,
+            "backend": "facebook",
+            "token": FBSDKAccessToken.current().tokenString,
+            "user_type": userType
+        ]
+        
+        Alamofire.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil).responseJSON { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                
+                let jsonData = JSON(value)
+                
+                self.accessToken = jsonData["access_token"].string!
+                self.refreshToken = jsonData["refresh_token"].string!
+                self.expired = Date().addingTimeInterval(TimeInterval(jsonData["expires_in"].int!))
+                
+                completionHandler(nil)
+                break
+                
+            case .failure(let error):
+                completionHandler(error as NSError?)
+                break
+            }
+        }
     }
     
     // API to log a user out
     func logout(completionHandler: @escaping (NSError?) -> Void) {
     
+        let path = "api/social/revoke-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String: Any] = [
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "token": self.accessToken
+        ]
+        
+        Alamofire.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil)
+            .responseString { (response) in
+                switch response.result {
+                case .success:
+                    completionHandler(nil)
+                    break
+                    
+                case .failure(let error):
+                    completionHandler(error as NSError?)
+                    break
+                }
+            }
+        
     }
     
 }
---
+++ constants.swift
@@ -0,0 +1,16 @@
+//
+//  constants.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/4/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://localhost:8000"
+let CLIENT_ID: String = "KtykRfbKkLAm1DQi81TuXXzmJXDBOzelpOBCY8nX"
+let CLIENT_SECRET: String = "ZYMejeMvBQjFwia2CRwVl1El27PMpVeJIxshRAKFMdc3DPYYHEECpzR8iX7i9ya09mDIWUqzomiCjjvRywYiw21DuPoPbbCp92QRgNEoyDCeC9dXWvujuKbIjHcCq9tb"
+
+let USERTYPE_CUSTOMER = "customer"
+let USERTYPE_DRIVER = "driver"
