d0bfe93d8f0a952ffa2ab21abf4de665779aaba0
matthew-gordon <laxredemption5525@gmail.com>
2017-03-04 15:14:22 -0700

    Add updating bLogin, round out log out functionality

--- dash-and-dine-mobile/CustomerMenuTableViewController.swift
+++ dash-and-dine-mobile/CustomerMenuTableViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class CustomerMenuTableViewController: UITableViewController {
 
+    @IBOutlet weak var imgAvatar: UIImageView!
+    @IBOutlet weak var lbName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        lbName.text = User.currentUser.name
+        imgAvatar.image = try! UIImage(data: Data(contentsOf: URL(string: User.currentUser.pictureURL!)!))
+        imgAvatar.layer.cornerRadius = 70 / 2
+        imgAvatar.layer.borderWidth = 1.0
+        imgAvatar.layer.borderColor = UIColor.white.cgColor
+        imgAvatar.clipsToBounds = true
+        
         view.backgroundColor = UIColor(red:0.04, green:0.55, blue:0.94, alpha:1.0)
     }
     
--- dash-and-dine-mobile/LoginViewController.swift
+++ dash-and-dine-mobile/LoginViewController.swift
@@ -11,18 +11,40 @@ import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var bLogin: UIButton!
+    @IBOutlet weak var bLogout: UIButton!
+    
     var fbLoginSuccess = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if (FBSDKAccessToken.current() != nil) {
+            
+            bLogout.isHidden = false
+            FBManager.getFBUserData(completionHandler: { 
+                
+                self.bLogin.setTitle("Continue as \(User.currentUser.email!)", for: .normal)
+                // self.bLogin.sizeToFit()
+            })
+        }
         
+        bLogin.layer.cornerRadius = 5
     }
     
     override func viewDidAppear(_ animated: Bool) {
         if (FBSDKAccessToken.current() != nil && fbLoginSuccess == true) {
             performSegue(withIdentifier: "CustomerView", sender: self)
         }
+    }
+    
+    @IBAction func facebookLogout(_ sender: Any) {
+        
+        FBManager.shared.logOut()
+        User.currentUser.resetInfo()
+        
+        bLogout.isHidden = true
+        bLogin.setTitle("Login with Facebook", for: .normal)
     }
     
     @IBAction func facebookLogin(_ sender: AnyObject) {
