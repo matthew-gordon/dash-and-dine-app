6c25ec6bc187ada200445175bb9698ae0c8baa71
matthew-gordon <laxredemption5525@gmail.com>
2017-03-05 20:28:54 -0700

    Add general tray view logic

--- dash-and-dine-mobile/TrayViewController.swift
+++ dash-and-dine-mobile/TrayViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var tbvMeals: UITableView!
+    @IBOutlet weak var viewTotal: UIView!
+    @IBOutlet weak var viewAddress: UIView!
+    @IBOutlet weak var viewMap: UIView!
+    @IBOutlet weak var lbTotal: UILabel!
+    @IBOutlet weak var textBoxAddress: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var bAddPayment: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,35 @@ class TrayViewController: UIViewController {
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        if Tray.currentTray.items.count == 0 {
+            // Show message here
+            
+            let lbEmptyTray = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 40))
+            lbEmptyTray.center = self.view.center
+            lbEmptyTray.textAlignment = NSTextAlignment.center
+            lbEmptyTray.text = "Your tray is currently empty. Please select a meal."
+            
+            self.view.addSubview(lbEmptyTray)
+            
+        } else {
+        
+            // Display all of the UI controls
+            
+            self.tbvMeals.isHidden = false
+            self.viewTotal.isHidden = false
+            self.viewAddress.isHidden = false
+            self.viewMap.isHidden = false
+            self.bAddPayment.isHidden = false
+            
+            loadMeals()
+        }
+    }
+    
+    func loadMeals() {
+        self.tbvMeals.reloadData()
+        self.lbTotal.text = "$\(Tray.currentTray.getTotal())"
+        
     }
 }
 
