7d2e7542d1760b7594fd7b77338da4949fe66307
matthew-gordon <laxredemption5525@gmail.com>
2017-03-05 17:29:41 -0700

    Add meal details

--- dash-and-dine-mobile/MealDetailsViewController.swift
+++ dash-and-dine-mobile/MealDetailsViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class MealDetailsViewController: UIViewController {
 
+    @IBOutlet weak var imgMealImage: UIImageView!
+    @IBOutlet weak var lbMealName: UILabel!
+    @IBOutlet weak var lbMealShortDescription: UILabel!
+    
+    var meal: Meal?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadMeal()
+    }
+    
+    func loadMeal() {
+    
+        lbMealName.text = meal?.name
+        lbMealShortDescription.text = meal?.short_description
         
+        if let imageUrl = meal?.image {
+            
+            Helpers.loadImage(imgMealImage, "\(imageUrl)")
+        }
     }
 }
