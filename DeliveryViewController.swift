44f0c96a39a0be6e87edd0ffbd7b4ed7dc2c5a59
matthew-gordon <laxredemption5525@gmail.com>
2017-03-06 23:58:46 -0700

    Add delivery map view

--- DeliveryViewController.swift
+++ DeliveryViewController.swift
@@ -22,6 +22,12 @@ class DeliveryViewController: UIViewController {
     
     var orderId: Int?
     
+    var destination: MKPlacemark?
+    var source: MKPlacemark?
+    
+    
+    let activityIndicator = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -40,7 +46,7 @@ class DeliveryViewController: UIViewController {
     func loadData() {
     
         APIManager.shared.getCurrentDriverOrder { (json) in
-            
+            Helpers.showActivityIndicator(self.activityIndicator, self.view)
 //            print(json)
             let order = json["order"]
             
@@ -60,6 +66,17 @@ class DeliveryViewController: UIViewController {
                 self.imgCustomerAvatar.image = try! UIImage(data: Data(contentsOf: URL(string: customerAvatar)!))
                 self.imgCustomerAvatar.layer.cornerRadius = 50/2
                 self.imgCustomerAvatar.clipsToBounds = true
+                
+                self.getLocation(from, "Customer", { (src) in
+                    self.source = src
+                    
+                    self.getLocation(to, "Restaurant", { (dest) in
+                        self.destination = dest
+                        self.getDirections()
+                    })
+                })
+                
+                Helpers.hideActivityIndicator(self.activityIndicator)
             } else {
             
                 self.map.isHidden = true
@@ -74,8 +91,93 @@ class DeliveryViewController: UIViewController {
                 lbMessage.text = "You don't have any orders for delivery."
                 
                 self.view.addSubview(lbMessage)
-                
+                Helpers.hideActivityIndicator(self.activityIndicator)
             }
         }
     }
+}
+
+extension DeliveryViewController: MKMapViewDelegate {
+    
+    // #1 - Delegate method of MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 5.0
+        
+        return renderer
+    }
+    
+    // #2 - Convert an address string to a location on the map
+    func getLocation(_ address: String,_ title: String,_ completionHandler: @escaping (MKPlacemark) -> Void) {
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            
+            if (error != nil) {
+                print("Error: ", error!)
+            }
+            
+            if let placemark = placemarks?.first {
+                
+                let coordinates: CLLocationCoordinate2D = placemark.location!.coordinate
+                
+                // Create a pin
+                let dropPin = MKPointAnnotation()
+                dropPin.coordinate = coordinates
+                dropPin.title = title
+                
+                self.map.addAnnotation(dropPin)
+                completionHandler(MKPlacemark.init(placemark: placemark))
+            }
+        }
+    }
+    
+    // #3 - Get direction and zoom to address
+    func getDirections() {
+        
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem.init(placemark: source!)
+        request.destination = MKMapItem.init(placemark: destination!)
+        request.requestsAlternateRoutes = false
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { (response, error) in
+            
+            if error != nil {
+                print("Error: ", error!)
+            } else {
+                // Show route
+                self.showRoute(response: response!)
+            }
+        }
+        
+    }
+    
+    // #4 - Show route between locations and make a visible zoom
+    func showRoute(response: MKDirectionsResponse) {
+        
+        for route in response.routes {
+            self.map.add(route.polyline, level: MKOverlayLevel.aboveRoads)
+        }
+        
+        var zoomRect = MKMapRectNull
+        for annotation in self.map.annotations {
+            let annotationPoint = MKMapPointForCoordinate(annotation.coordinate)
+            let pointRect = MKMapRectMake(annotationPoint.x, annotationPoint.y, 0.1, 0.1)
+            zoomRect = MKMapRectUnion(zoomRect, pointRect)
+        }
+        
+        let insetWidth = -zoomRect.size.width * 0.2
+        let insetHeight = -zoomRect.size.height * 0.2
+        let insetRect = MKMapRectInset(zoomRect, insetWidth, insetHeight)
+        
+        self.map.setVisibleMapRect(insetRect, animated: true)
+    }
+    
+    
+    
 }
