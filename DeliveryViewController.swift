c376804d4a9e5fd4bb144b224509d2094ec21b74
matthew-gordon <laxredemption5525@gmail.com>
2017-03-06 19:54:18 -0700

    Add Delivery view/controller

---
+++ DeliveryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DeliveryViewController.swift
+//  dash-and-dine-mobile
+//
+//  Created by Matt Gordon on 3/6/17.
+//  Copyright © 2017 Matt Gordon. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryViewController: UIViewController {
+
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
